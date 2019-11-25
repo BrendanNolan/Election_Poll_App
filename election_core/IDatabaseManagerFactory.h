8037d3018dc95973bb148b45c119464dcc516257
BrendanNolan <bnolan23@gmail.com>
2019-11-25 21:29:31 +0000

    Added code for IDatabaseManagerFactory

--- election_core/IDatabaseManagerFactory.h
+++ election_core/IDatabaseManagerFactory.h
@@ -1,6 +1,21 @@
 #ifndef IDATABASEMANAGERFACTORY_H
 #define IDATABASEMANAGERFACTORY_H
 
+#include <memory>
+
+class IConstituencyDatabaseManager;
+class IPollResultDatabaseManager;
+class IImageDatabaseManager;
+
 class IDatabaseManagerFactory
+{
+public:
+    virtual std::shared_ptr<IConstituencyDatabaseManager> 
+        createConstituencyDatabaseManager() = 0;
+    virtual std::shared_ptr<IPollResultDatabaseManager>
+        createPollResultDatabaseManager() = 0;
+    virtual std::shared_ptr<IImageDatabaseManager>
+        createImageDatabaseManager() = 0;
+};
 
 #endif //IDATABASEMANAGERFACTORY_H

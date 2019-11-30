6e2e2909b41b863f3aafb70fa5c1845b5b65f858
BrendanNolan <bnolan23@gmail.com>
2019-11-30 13:20:59 +0000

    Added code for ConstituencyModel

--- election_core/ConstituencyModel.cpp
+++ election_core/ConstituencyModel.cpp
@@ -1,1 +1,13 @@
 #include "ConstituencyModel.h"
+
+QVariant ConstituencyModel::data(const QModelIndex &index, int role) const
+{
+    auto row = index.row();
+    // to be continued ...
+    // put a switch in here probs
+}
+
+int ConstituencyModel::rowCount() const
+{
+    return constituencies_.size();
+}
--- election_core/ConstituencyModel.h
+++ election_core/ConstituencyModel.h
@@ -3,9 +3,30 @@
 
 #include <QAbstractTableModel>
 
-class ConstituencyModel : public QAbstractTableModel
-{
+#include <vector>
 
+#include "Constituency.h"
+
+
+class ConstituencyModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum class Role
+    {
+        LatitudeRole = Qt::UserRole + 1,
+        NameRole,
+        CurrentPartyRole,
+        CurrentMPRole
+    };
+
+    QVariant data(
+        const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    int rowCount() const;
+
+private:
+    std::vector<Constituency> constituencies_;
 };
 
 #endif // CONSTITUENCYMODEL_H

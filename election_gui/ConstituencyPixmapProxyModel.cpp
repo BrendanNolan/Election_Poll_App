2882320bdf62dbac4ffefc4da4feee83f0875d82
BrendanNolan <bnolan23@gmail.com>
2020-01-30 20:31:49 +0000

    Further developed implementation of ConstituencyPixmapProxymodel.

--- election_gui/ConstituencyPixmapProxyModel.cpp
+++ election_gui/ConstituencyPixmapProxyModel.cpp
@@ -33,7 +33,7 @@ void ConstituencyPixmapProxyModel::setSourceModel(
     connect(sourceModel, &QAbstractItemModel::dataChanged,
         [this](const QModelIndex& topLeft, const QModelIndex& bottomRight) {
             auto count = bottomRight.row() - topLeft.row();
-            if (!(count > 0))
+            if (count <= 0)
                 return;
             partiallyRefreshPixmapCache(topLeft, count);
         });
@@ -51,12 +51,19 @@ ConstituencyModel* ConstituencyPixmapProxyModel::constituencyModel() const
 }
 
 void ConstituencyPixmapProxyModel::partiallyRefreshPixmapCache(
-    const QModelIndex & startIndex, 
+    const QModelIndex& startIndex, 
     int count)
 {
-    if (!(count > 0))
+    if (count <= 0)
         return;
-
+    auto startRow = startIndex.row();
+    for (auto i = 0; i < count; ++i)
+    {
+        auto pixmapUpdateIndex = index(startRow + i, 0);
+        QPixmap pixmap(10, 10); // Will need to be more creative here in the end of course.
+        pixmap.fill(Qt::red);
+        constituencyPixmapCache_[pixmapUpdateIndex] = pixmap;
+    }
 }
 
 void ConstituencyPixmapProxyModel::refreshConstituencyPixmapCache()

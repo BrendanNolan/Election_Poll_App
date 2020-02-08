90b0987d4c5fc5d5bc9c54ff0aec5558eeecc54f
BrendanNolan <bnolan23@gmail.com>
2020-02-08 22:09:27 +0000

    Stopped variables defined in ElectionDefinitions.h from having external linkage.

--- election_core/ElectionDefinitions.h
+++ election_core/ElectionDefinitions.h
@@ -2,7 +2,7 @@
 
 namespace paths
 {
-    QString databasePath(
+    static const QString databasePath(
         "C:\\CPP_Stuff\\Election_Poll_App\\primary_database.db");
-    QString scraperScript("C:\\CPP_Stuff\\Election_Poll_App\\scraping.py");
+    static const QString scraperScript("C:\\CPP_Stuff\\Election_Poll_App\\scraping.py");
 }

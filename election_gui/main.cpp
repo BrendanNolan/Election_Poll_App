eacf27da53aaea6fba06e0c444ab0374602eb2e5
BrendanNolan <bnolan23@gmail.com>
2020-02-08 22:07:50 +0000

    (Tried to) set up Python interpreter.

--- election_gui/main.cpp
+++ election_gui/main.cpp
@@ -1,9 +1,21 @@
+#include <Python.h>
+
 #include <QApplication>
+
+#include <cstdlib>
+#include <string>
 
 #include "MainWindow.h"
 
 int main(int argc, char* argv[])
-{
+{   
+    // Setting up python interpreter
+    auto c = argv[0];
+    auto cSz = strlen(c);
+    std::wstring wc(cSz + 1, L'#');
+    std::mbstowcs(&wc[0], c, cSz);
+    Py_SetProgramName(wc.c_str());
+
     QApplication app(argc, argv);
     MainWindow mainWin;
     mainWin.show();

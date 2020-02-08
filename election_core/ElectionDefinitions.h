860564d5814ac57d42ffbca1202a04077b557acc
BrendanNolan <bnolan23@gmail.com>
2020-02-08 19:51:14 +0000

    COrrected spelling mistake in name of python script to be run from C++.

--- election_core/ElectionDefinitions.h
+++ election_core/ElectionDefinitions.h
@@ -4,5 +4,5 @@ namespace paths
 {
     QString databasePath(
         "C:\\CPP_Stuff\\Election_Poll_App\\primary_database.db");
-    QString scraperScript("C:\\CPP_Stuff\\Election_Poll_App\\scraper.py");
+    QString scraperScript("C:\\CPP_Stuff\\Election_Poll_App\\scraping.py");
 }

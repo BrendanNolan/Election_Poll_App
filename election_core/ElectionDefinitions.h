646ee003c72fbc125b8d29c6291ef98072f95ae8
BrendanNolan <bnolan23@gmail.com>
2020-02-12 23:10:56 +0000

    Reordered directories to make navigation easier.

--- election_core/ElectionDefinitions.h
+++ election_core/ElectionDefinitions.h
@@ -4,5 +4,5 @@ namespace paths
 {
     static const QString databasePath(
         "C:\\CPP_Stuff\\Election_Poll_App\\primary_database.db");
-    static const QString scraperScript("C:\\CPP_Stuff\\Election_Poll_App\\scraping.py");
+    static const QString scraperScript("C:\\CPP_Stuff\\Election_Poll_App\\Scripts\\scraping.py");
 }

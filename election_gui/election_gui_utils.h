ee1ee0bbcf6479550ba082bc14e1adac586a0206
BrendanNolan <bnolan23@gmail.com>
2020-02-15 13:35:24 +0000

    Started to add code to deal with polar coordinates.

--- election_gui/election_gui_utils.h
+++ election_gui/election_gui_utils.h
@@ -5,7 +5,40 @@ class QFileInfo;
 
 namespace python_script_running
 {
-    void runPythonScript(const QFileInfo& script);
+
+void runPythonScript(const QFileInfo& script);
+
+};
+
+namespace geom_utils
+{
+
+template <class T>
+struct PolarPoint
+{
+    T r_;
+    T theta_;
+};
+
+template <class T>
+struct CartesianPoint
+{
+    T x_;
+    T y_;
+};
+
+template <class T>
+PolarPoint<T> cartesianToPolar(const CartesianPoint<T>& cartPoint)
+{
+
+}
+
+template <class T>
+CartesianPoint<T> polarToCartesian(const PolarPoint<T>& polarPoint)
+{
+
+}
+
 };
 
 #endif // ELECTION_GUI_UTILS_H

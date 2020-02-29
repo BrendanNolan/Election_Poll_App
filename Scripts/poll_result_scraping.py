d43375e756e7c0d4d40bcfd2ca85d7d0f99f5a94
BrendanNolan <bnolan23@gmail.com>
2020-02-29 16:20:22 +0000

    Added skeletal beginning to poll_result_scraping.py

---
+++ Scripts/poll_result_scraping.py
@@ -0,0 +1,15 @@
+import csv
+import os
+from random import randrange
+import sqlite3
+
+
+path_to_pictures = "C:\\CPP_Stuff\\Election_Poll_App\\Pictures\\"
+
+primary_database_path = "C:\\CPP_Stuff\\Election_Poll_App\\primary_database.db"
+primary_database = open(primary_database_path, 'w')
+primary_database.close()
+
+db = sqlite3.connect(primary_database_path)
+cursor = db.cursor()
+

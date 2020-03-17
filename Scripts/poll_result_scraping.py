39a7cb3581f26ccbd211b4200a806e3801a28ee0
BrendanNolan <bnolan23@gmail.com>
2020-03-17 15:22:06 +0000

    Made first attempt at working version of poll_result_scraping.py.

--- Scripts/poll_result_scraping.py
+++ Scripts/poll_result_scraping.py
@@ -16,3 +16,26 @@ cursor = db.cursor()
 cursor.execute('''
     DROP TABLE IF EXISTS poll_results
 ''')
+cursor.execute('''
+    CREATE TABLE poll_results
+        (
+        constituency_id INTEGER,
+        source TEXT,
+        date_time DATETIME,
+        politician_name TEXT,
+        poll_value INT
+        )
+''')
+db.commit()
+
+sources = ["NYT", "WAPO", "HUFFPO"]
+
+for constituency_id in range(1, 5):
+    for j in range(3):
+        cursor.execute('''
+            INSERT INTO poll_results
+            (constituency_id, source, date_time, politician_name, poll_value)
+            VALUES
+            (?, ?, ?, ?, ?)
+        ''', (constituency_id, sources[j], 20200201 + i, "Politician" + str(i), i * 40))
+        db.commit()

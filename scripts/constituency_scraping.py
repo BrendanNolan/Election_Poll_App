cdeefbbd2801d3c4af53d58deb5a9705a8f4996b
brendan_m_nolan <bnolan23@gmail.com>
2020-03-29 19:20:19 +0100

    Reinstating scripts folder.

---
+++ scripts/constituency_scraping.py
@@ -0,0 +1,39 @@
+import csv
+import os
+from random import randrange
+import sqlite3
+
+
+path_to_pictures = "C:\\CPP_Stuff\\Election_Poll_App\\Pictures\\"
+
+primary_database_path = os.getenv("POLL_ZAPP") + "\\databases\\primary_database.db"
+primary_database = open(primary_database_path, 'a')
+primary_database.close()
+
+db = sqlite3.connect(primary_database_path)
+cursor = db.cursor()
+
+cursor.execute('''
+    DROP TABLE IF EXISTS constituencies
+''')
+cursor.execute('''
+    CREATE TABLE constituencies  
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT, 
+        latitude INTEGER, 
+        longitude INTEGER
+        )
+''')
+db.commit()
+
+constituency_names = ["Wexford", "Dublin West", "Dun Laoighre", "Galway"]
+
+for i in range(len(constituency_names)):
+    cursor.execute('''
+        INSERT INTO constituencies 
+            (name, latitude, longitude) 
+            VALUES  
+            (?, ?, ?)
+    ''', (constituency_names[i], i * 40, i * 40))
+    db.commit()
---
+++ scripts/poll_result_scraping.py
@@ -0,0 +1,41 @@
+import csv
+import os
+from random import randrange
+import sqlite3
+
+
+path_to_pictures = "C:\\CPP_Stuff\\Election_Poll_App\\Pictures\\"
+
+primary_database_path = os.getenv("POLL_ZAPP") + "\\databases\\primary_database.db"
+primary_database = open(primary_database_path, 'a')
+primary_database.close()
+
+db = sqlite3.connect(primary_database_path)
+cursor = db.cursor()
+
+cursor.execute('''
+    DROP TABLE IF EXISTS poll_results
+''')
+cursor.execute('''
+    CREATE TABLE poll_results
+        (
+        constituency_id INTEGER,
+        source TEXT,
+        date_time DATETIME,
+        politician_name TEXT,
+        poll_value INT
+        )
+''')
+db.commit()
+
+sources = ["NYT", "WAPO", "HUFFPO"]
+
+for constituency_id in range(1, 5):
+    for j in range(3):
+        cursor.execute('''
+            INSERT INTO poll_results
+            (constituency_id, source, date_time, politician_name, poll_value)
+            VALUES
+            (?, ?, ?, ?, ?)
+        ''', (constituency_id, sources[randrange(3)], 20200201 + j, "Politician" + str(j), j * 20))
+        db.commit()

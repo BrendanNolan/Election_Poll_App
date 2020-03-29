23863715670871612e5cf9b49724e4b46f02591b
brendan_m_nolan <bnolan23@gmail.com>
2020-03-29 19:15:22 +0100

    Added comment to StartupHandler.h.

--- cpp_code/election_gui/StartupHandler.h
+++ cpp_code/election_gui/StartupHandler.h
@@ -1,6 +1,10 @@
 #ifndef STARTUPHANDLER_H
 #define STARTUPHANDLER_H
 
+/*
+    There is possibly some less-than-good code in here but since I'm using it 
+    temporarily and only in lieu of an installer, I guess that's ok. 
+*/
 class StartupHandler
 {
 public:

0d078545bacfeab54321f3130b09c0895ac85cca
BrendanNolan <bnolan23@gmail.com>
2020-03-29 14:42:53 +0100

    Updated python scripts to use environment variables rather than hard-coded paths.

--- Scripts/politician_scraping.py
+++ Scripts/politician_scraping.py
@@ -6,7 +6,7 @@ import sqlite3
 
 path_to_pictures = "C:\\CPP_Stuff\\Election_Poll_App\\Pictures\\"
 
-primary_database_path = "C:\\CPP_Stuff\\Election_Poll_App\\primary_database.db"
+primary_database_path = os.getenv("POLL_ZAPP") + "\\databases\\primary_database.db"
 primary_database = open(primary_database_path, 'a')
 primary_database.close()
 

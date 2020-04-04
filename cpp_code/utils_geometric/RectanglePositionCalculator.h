46d331b0ebe517bc0c912aa1b79b4f9d055fd11b
BrendanNolan <bnolan23@gmail.com>
2020-04-04 16:45:15 +0100

    Changes to RectanglePositionCalculator.h.

--- cpp_code/utils_geometric/RectanglePositionCalculator.h
+++ cpp_code/utils_geometric/RectanglePositionCalculator.h
@@ -1,18 +1,18 @@
 #ifndef RECTANGLEPOSITIONCALCULATOR_H
 #define RECTANGLEPOSITIONCALCULATOR_H
 
-// May be possible to use a forward declaration rather than including QRect and
-// QVector
-#include <QRect>
-#include <QVector>
+#include <vector>
+
+#include "Rectangle.h"
 
 class RectanglePositionCalculator
 {
 public:
-    RectanglePositionCalculator(const QVector<QRect>& rectangles);
-    QRect reposition(const QRect& rect)
-        const;// Should return a copy of its argument if it is passed a QRct
-              // that was not in its initialising QVector of QRects
+    void setPositions(std::vector<Rectangle>& rectangles);
+
+private:
+    void reposition(Rectangle& rect) const;
+    std::vector<Rectangle>* rectangles;
 };
 
 #endif// RECTANGLEPOSITIONCALCULATOR_H

3e1c66d509ad5236bdbd9e66022f06b4dd02b181
BrendanNolan <bnolan23@gmail.com>
2020-04-05 16:55:15 +0100

    Added QtSceneCoordConverter class.

---
+++ cpp_code/poll_zapp_gui/QtSceneCoordConverter.cpp
@@ -0,0 +1,36 @@
+#include "QtSceneCoordConverter.h"
+
+#include <cmath>
+
+#include "geom_defs.h"
+
+using namespace geom;
+
+QtSceneCoordConverter::QtSceneCoordConverter(double scale)
+    : scale_(scale)
+{
+}
+
+QPointF QtSceneCoordConverter::qPointF(const geom::Point& point) const
+{
+    auto scale = scale_;
+    if (std::abs(scale) < geom::doublePrecisionTolerance)
+        scale = 1;
+
+    return QPointF(point.x() / scale, -(point.y() / scale));
+}
+
+geom::Point QtSceneCoordConverter::point(const QPointF& qPointF) const
+{
+    auto scale = scale_;
+    if (std::abs(scale) < geom::doublePrecisionTolerance)
+        scale = 1;
+
+    return Point::newCartesianPoint(
+        qPointF.x() * scale, -(qPointF.y() * scale));
+}
+
+void QtSceneCoordConverter::setScale(double scale)
+{
+    scale_ = scale;
+}
---
+++ cpp_code/poll_zapp_gui/QtSceneCoordConverter.h
@@ -0,0 +1,36 @@
+#ifndef QTSCENECOORDCONVERTER_H
+#define QTSCENECOORDCONVERTER_H
+
+#include <QPointF>
+#include "Point.h"
+
+/*
+    At some time, must make sure to use this class whenever we are converting
+    between normal coordinates and QGraphicsScene coordinates (where the y-axis
+    is inverted).
+*/
+
+class QtSceneCoordConverter
+{
+public:
+    /*
+        A distance of 1 between two geom::Point objects is represented by a 
+        distance of 1/scale on the QGraphicsScene.
+
+        In other words, scale is the shrinkage factor when going from a geom::Point to 
+        a QPointF. This will be very large if, for example, your QGraphicsScene
+        is displaying a map of the world and the geom::Point objects represnting
+        points on the globe are measured in milimetres.
+    */
+    QtSceneCoordConverter(double scale = 1);
+
+    QPointF qPointF(const geom::Point& point) const;
+    geom::Point point(const QPointF& qPointF) const;
+
+    void setScale(double scale);
+
+private:
+    double scale_ = 1.0;
+};
+
+#endif// QTSCENECOORDCONVERTER_H

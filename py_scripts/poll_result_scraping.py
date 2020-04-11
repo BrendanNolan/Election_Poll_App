b2cb5babf806967fae3c72cfae0a2031afbe1630
BrendanNolan <bnolan23@gmail.com>
2020-04-11 12:26:23 +0100

    Updated py_scripts/poll_result_scraping.py

--- py_scripts/poll_result_scraping.py
+++ py_scripts/poll_result_scraping.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from random import randrange
+import random
 import sqlite3
 
 
@@ -28,14 +28,22 @@ cursor.execute('''
 ''')
 db.commit()
 
-sources = ["NYT", "WAPO", "HUFFPO"]
+sources = ["NYT", "WAPO", "HUFFPO", "NYPOST"]
+politician_names = ["Rory", "David", "Kate", "Brendan"]
+
+source_range = list(range(4))
+politician_range = list(range(4))
+random.shuffle(source_range)
+random.shuffle(politician_range)
 
 for constituency_id in range(1, 5):
-    for j in range(3):
-        cursor.execute('''
-            INSERT INTO poll_results
-            (constituency_id, source, date_time, politician_name, poll_value)
-            VALUES
-            (?, ?, ?, ?, ?)
-        ''', (constituency_id, sources[randrange(3)], 20200201 + j, "Politician" + str(j), j * 20))
-        db.commit()
+    for j in source_range:
+        for i in politician_range:
+            cursor.execute('''
+                INSERT INTO poll_results
+                (constituency_id, source, date_time, politician_name, poll_value)
+                VALUES
+                (?, ?, ?, ?, ?)
+            ''', (constituency_id, sources[j], 20200201 + j, politician_names[i], j * 20))
+
+db.commit()
